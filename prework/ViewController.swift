4bcba196de7d51f94b74924d7934d212e04495a6
smanasreh6 <smanasreh6@gatech.edu>
2024-12-17 22:56:54 -0500

    w

--- prework/ViewController.swift
+++ prework/ViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func changeTextColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.tintColor = randomColor
+    }
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
